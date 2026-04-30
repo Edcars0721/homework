85ee532e45d0a1498f53f1f7d31c8b99246bded8
Edcars0721 <Edcars0721@example.com>
2026-04-30 22:26:01 +0800

    Second commit: Add input function

--- ex01/hello.cpp
+++ ex01/hello.cpp
@@ -1,5 +1,9 @@
 #include <stdio.h>
 int main() {
+    char input[100];  // 用来存你输入的内容
     printf("Hello, World!\n");
+    printf("请输入内容：");
+    scanf("%s", input);  // 读取键盘输入
+    printf("你输入的是：%s\n", input);  // 把输入的内容显示出来
     return 0;
 }
